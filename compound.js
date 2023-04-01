fd1af516a3735d8e942401daf794143def868d57
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2023-03-31 22:55:16 -0300

    button calculate working

--- compound.js
+++ compound.js
@@ -1,12 +1,8 @@
 // getting values from input
 const principal = document.querySelector("#principal")
-// console.log(principal.value);
 const monthly = document.querySelector("#monthly")
-// console.log(monthly.value);
 const months = document.querySelector("#months")
-// console.log(months.value);
 const rate = document.querySelector("#rate")
-// console.log(rate.value);
 
 // setting variables for formula for better understanding 
 /*
@@ -19,17 +15,36 @@ t = time invested (in months)
 PMT = addition monthly 
 
 */
-// create numbers:
+
+
+function calculate(){
+    // create numbers:
 const P = parseInt(principal.value);
 const r = parseFloat(rate.value);
 const n = 12;
 const t = parseInt(months.value);
 const PMT = parseInt(monthly.value);
-
 const CI = P * (1 + (r / n)) ** (n * t/12) + (PMT * ((Math.pow(1 + (r / n), n * t/12) - 1)) / (r / n));
 const formattedCI = CI.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
-console.log(formattedCI);
 
+const result = document.querySelector(".result")
+result.innerText  = formattedCI;
 
+// how much were invested 
+const investedAmount = P + t * PMT
+const formattedCIInvestedAmount = investedAmount.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+const invested = document.querySelector(".invested")
+invested.innerText = formattedCIInvestedAmount;
+// how much was the interest
+const interestAmount = CI - investedAmount
+const formattedCIinterestAmount = interestAmount.toLocaleString('en-US', {style: 'currency', currency: 'USD'});
+const interest = document.querySelector('.interest')
+interest.innerText = formattedCIinterestAmount;
 
-  
+}
+
+const button = document.querySelector('.btn')
+
+button.addEventListener('click', ()=>{
+    calculate()
+})
